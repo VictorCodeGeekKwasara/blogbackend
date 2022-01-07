3ba561f098f0cb15247327773a7c1d8f00edaf3c
Victor Kwasara <kwasaravictort@gmail.com>
2022-01-07 15:37:59 +0200

    bootstrap the backend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-require('dontenv').config();
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
 	res.send('Hie from the internet');
