ecad3c7e1f0d5ed266f8d09fba29f6e79a773819
Victor Kwasara <kwasaravictort@gmail.com>
2022-01-07 15:54:48 +0200

    database connection

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const port = process.env.PORT;
+const mongoose = require('mongoose');
 
+const port = process.env.PORT;
+const dbURI = process.env.DB_URI;
+
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) =>
+		app.listen(port, () => {
+			console.log(`listening`);
+		})
+	)
+	.catch((err) => console.log(err));
 app.get('/', (req, res) => {
 	res.send('Hie from the internet');
-});
-
-app.listen(port, () => {
-	console.log(`listening`);
 });
