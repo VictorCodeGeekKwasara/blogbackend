53f57581a69ec118f8e34ff10b9c7a6e934d195c
Victor Kwasara <kwasaravictort@gmail.com>
2022-01-08 19:38:20 +0200

    route error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { signup_post } = require('../controllers/authController');
 const { login_post } = require('../controllers/authController');
+const { logout_get } = require('../controllers/authController');
 
 const router = Router();
 
 router.post('/signup', signup_post);
 router.post('/login', login_post);
-router.get('/logout', authController.logout_get);
+router.get('/logout', logout_get);
 module.exports = router;
